c890441282a26d50638c05cb839d93c901b46d4d
DaMilks <danya.boldyrev.89@mail.ru>
2024-09-23 04:59:57 +0300

    Добавьте файлы проекта.

---
+++ FDM/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace FDM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.Chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.Chart.Legends.Add(legend1);
+            this.Chart.Location = new System.Drawing.Point(12, 12);
+            this.Chart.Name = "Chart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.Chart.Series.Add(series1);
+            this.Chart.Size = new System.Drawing.Size(641, 300);
+            this.Chart.TabIndex = 0;
+            this.Chart.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Chart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart Chart;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FDM/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace FDM
+{
+    public partial class Form1 : Form
+    {
+        
+        Timer timer = new Timer();
+        double time=0, temp;
+        static int numpoints = 100;
+        (double X, double Y)[] data = new (double, double)[numpoints];
+        static double L = 10, T = 300;
+        static double stepX = L / numpoints;
+        double stepT = 0.5 * stepX*stepX;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private double Func(double t)
+        {
+            return 300 + 2 * Math.Sin(2 * Math.PI * 7 * t);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Chart.ChartAreas[0].AxisY.Minimum = 290;
+            Chart.ChartAreas[0].AxisY.Maximum = 310;
+            for (int i = 0; i < numpoints; i++)
+            {
+
+                data[i] = (i*stepX, T);
+                Chart.Series[0].Points.AddXY(data[i].X, data[i].Y);
+            }
+            timer.Interval = 100;
+            timer.Tick += (o, args) => timer_Tick(o,args);
+            timer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            time += stepT;
+            data[0] = (0, Func(time));
+            Chart.Series[0].Points.Clear();
+            for(int i = 1; i < numpoints-1; i++)
+            {
+                data[i]=(i*stepX,  0.5*data[i - 1].Y + 0.5 * data[i + 1].Y);
+                Chart.Series[0].Points.AddXY(data[i].X, data[i].Y);
+            }
+                
+        }
+        }
+    }
