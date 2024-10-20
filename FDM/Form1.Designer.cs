4d202a14d8e768a1f42589480f67c57e416492df
DaMilks <danya.boldyrev.89@mail.ru>
2024-10-20 20:17:48 +0300

    Implict

--- FDM/Form1.Designer.cs
+++ FDM/Form1.Designer.cs
@@ -32,13 +32,34 @@
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.ChartTX = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
             this.ChartTt = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.TextBoxAmplitude = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Un = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ChartTX1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.TextBoxAmplitude1 = new System.Windows.Forms.TextBox();
+            this.ChartTt1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBoxT = new System.Windows.Forms.TextBox();
+            this.textBoxS = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBoxPrint = new System.Windows.Forms.CheckBox();
+            this.checkBoxPrint1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.ChartTX)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ChartTt)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.Un.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartTX1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartTt1)).BeginInit();
             this.SuspendLayout();
             // 
             // ChartTX
@@ -53,7 +74,7 @@
             chartArea1.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
             chartArea1.Name = "ChartArea1";
             this.ChartTX.ChartAreas.Add(chartArea1);
-            this.ChartTX.Location = new System.Drawing.Point(12, 12);
+            this.ChartTX.Location = new System.Drawing.Point(6, 21);
             this.ChartTX.Name = "ChartTX";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
@@ -63,15 +84,16 @@
             this.ChartTX.TabIndex = 0;
             this.ChartTX.Text = "chart1";
             // 
-            // button1
+            // buttonStart
             // 
-            this.button1.Location = new System.Drawing.Point(12, 318);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Старт";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(1309, 33);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(102, 38);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.button1_Click);
             // 
             // ChartTt
             // 
@@ -84,7 +106,7 @@
             chartArea2.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
             chartArea2.Name = "ChartArea1";
             this.ChartTt.ChartAreas.Add(chartArea2);
-            this.ChartTt.Location = new System.Drawing.Point(659, 12);
+            this.ChartTt.Location = new System.Drawing.Point(653, 21);
             this.ChartTt.Name = "ChartTt";
             series2.ChartArea = "ChartArea1";
             series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
@@ -94,50 +116,225 @@
             this.ChartTt.TabIndex = 2;
             this.ChartTt.Text = "chart1";
             // 
-            // button2
+            // buttonStop
             // 
-            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button2.Location = new System.Drawing.Point(93, 318);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(104, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Стоп";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.buttonStop.Cursor = System.Windows.Forms.Cursors.Default;
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStop.Location = new System.Drawing.Point(1309, 77);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(102, 38);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "Стоп";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox1
+            // TextBoxAmplitude
             // 
-            this.textBox1.Location = new System.Drawing.Point(203, 318);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(181, 22);
-            this.textBox1.TabIndex = 4;
+            this.TextBoxAmplitude.Location = new System.Drawing.Point(115, 330);
+            this.TextBoxAmplitude.Name = "TextBoxAmplitude";
+            this.TextBoxAmplitude.ReadOnly = true;
+            this.TextBoxAmplitude.Size = new System.Drawing.Size(181, 26);
+            this.TextBoxAmplitude.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBoxPrint);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.ChartTX);
+            this.groupBox1.Controls.Add(this.TextBoxAmplitude);
+            this.groupBox1.Controls.Add(this.ChartTt);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1291, 364);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Метод сеток";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 333);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Амплитуда";
+            // 
+            // Un
+            // 
+            this.Un.Controls.Add(this.checkBoxPrint1);
+            this.Un.Controls.Add(this.label5);
+            this.Un.Controls.Add(this.label4);
+            this.Un.Controls.Add(this.textBoxS);
+            this.Un.Controls.Add(this.textBoxT);
+            this.Un.Controls.Add(this.label2);
+            this.Un.Controls.Add(this.ChartTX1);
+            this.Un.Controls.Add(this.TextBoxAmplitude1);
+            this.Un.Controls.Add(this.ChartTt1);
+            this.Un.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Un.Location = new System.Drawing.Point(12, 382);
+            this.Un.Name = "Un";
+            this.Un.Size = new System.Drawing.Size(1291, 425);
+            this.Un.TabIndex = 6;
+            this.Un.TabStop = false;
+            this.Un.Text = "Метод сеток неявный";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Амплитуда";
+            // 
+            // ChartTX1
+            // 
+            chartArea3.AxisX.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Horizontal;
+            chartArea3.AxisX.Title = "X";
+            chartArea3.AxisX.TitleAlignment = System.Drawing.StringAlignment.Far;
+            chartArea3.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            chartArea3.AxisY.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Horizontal;
+            chartArea3.AxisY.Title = "T";
+            chartArea3.AxisY.TitleAlignment = System.Drawing.StringAlignment.Far;
+            chartArea3.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            chartArea3.Name = "ChartArea1";
+            this.ChartTX1.ChartAreas.Add(chartArea3);
+            this.ChartTX1.Location = new System.Drawing.Point(6, 21);
+            this.ChartTX1.Name = "ChartTX1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Name = "Series1";
+            this.ChartTX1.Series.Add(series3);
+            this.ChartTX1.Size = new System.Drawing.Size(641, 300);
+            this.ChartTX1.TabIndex = 0;
+            this.ChartTX1.Text = "chart1";
+            // 
+            // TextBoxAmplitude1
+            // 
+            this.TextBoxAmplitude1.Location = new System.Drawing.Point(115, 330);
+            this.TextBoxAmplitude1.Name = "TextBoxAmplitude1";
+            this.TextBoxAmplitude1.ReadOnly = true;
+            this.TextBoxAmplitude1.Size = new System.Drawing.Size(181, 26);
+            this.TextBoxAmplitude1.TabIndex = 4;
+            // 
+            // ChartTt1
+            // 
+            chartArea4.AxisX.Title = "t";
+            chartArea4.AxisX.TitleAlignment = System.Drawing.StringAlignment.Far;
+            chartArea4.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            chartArea4.AxisY.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Horizontal;
+            chartArea4.AxisY.Title = "T";
+            chartArea4.AxisY.TitleAlignment = System.Drawing.StringAlignment.Far;
+            chartArea4.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            chartArea4.Name = "ChartArea1";
+            this.ChartTt1.ChartAreas.Add(chartArea4);
+            this.ChartTt1.Location = new System.Drawing.Point(653, 21);
+            this.ChartTt1.Name = "ChartTt1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Name = "Series1";
+            this.ChartTt1.Series.Add(series4);
+            this.ChartTt1.Size = new System.Drawing.Size(616, 300);
+            this.ChartTt1.TabIndex = 2;
+            this.ChartTt1.Text = "chart1";
+            // 
+            // textBoxT
+            // 
+            this.textBoxT.Location = new System.Drawing.Point(364, 333);
+            this.textBoxT.Name = "textBoxT";
+            this.textBoxT.Size = new System.Drawing.Size(181, 26);
+            this.textBoxT.TabIndex = 8;
+            // 
+            // textBoxS
+            // 
+            this.textBoxS.Location = new System.Drawing.Point(364, 365);
+            this.textBoxS.Name = "textBoxS";
+            this.textBoxS.ReadOnly = true;
+            this.textBoxS.Size = new System.Drawing.Size(181, 26);
+            this.textBoxS.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(344, 333);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "t";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(344, 368);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "s";
+            // 
+            // checkBoxPrint
+            // 
+            this.checkBoxPrint.AutoSize = true;
+            this.checkBoxPrint.Location = new System.Drawing.Point(302, 333);
+            this.checkBoxPrint.Name = "checkBoxPrint";
+            this.checkBoxPrint.Size = new System.Drawing.Size(155, 24);
+            this.checkBoxPrint.TabIndex = 6;
+            this.checkBoxPrint.Text = "Отрисовывать";
+            this.checkBoxPrint.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxPrint1
+            // 
+            this.checkBoxPrint1.AutoSize = true;
+            this.checkBoxPrint1.Location = new System.Drawing.Point(10, 356);
+            this.checkBoxPrint1.Name = "checkBoxPrint1";
+            this.checkBoxPrint1.Size = new System.Drawing.Size(155, 24);
+            this.checkBoxPrint1.TabIndex = 7;
+            this.checkBoxPrint1.Text = "Отрисовывать";
+            this.checkBoxPrint1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1303, 379);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.ChartTt);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.ChartTX);
+            this.ClientSize = new System.Drawing.Size(1464, 836);
+            this.Controls.Add(this.Un);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.ChartTX)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ChartTt)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.Un.ResumeLayout(false);
+            this.Un.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartTX1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartTt1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart ChartTX;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.DataVisualization.Charting.Chart ChartTt;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox TextBoxAmplitude;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox Un;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ChartTX1;
+        private System.Windows.Forms.TextBox TextBoxAmplitude1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ChartTt1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxS;
+        private System.Windows.Forms.TextBox textBoxT;
+        private System.Windows.Forms.CheckBox checkBoxPrint;
+        private System.Windows.Forms.CheckBox checkBoxPrint1;
     }
 }
 
--- FDM/Form1.cs
+++ FDM/Form1.cs
@@ -1,26 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
 
 namespace FDM
 {
     public partial class Form1 : Form
     {
-        
+
         Timer timer = new Timer();
-        double time=0, previosTemp,previosTemp1,MinTemp=350,Maxtemp=0;
+        double time = 0, previosTemp, previosTemp1, MinTemp, Maxtemp, MinTemp1, Maxtemp1;
+
+        double time1 = 0, stepT1, s;
+        double[] a = new double[numpoints], b = new double[numpoints];
+
         static int numpoints = 100;
         double[] data = new double[numpoints];
-        static double L = 10, T = 300;
+        double[] data1 = new double[numpoints];
+        static double L = 10, T = 300, D = 2;
         static double stepX = L / numpoints;
-        double stepT = 0.5 * stepX*stepX;
+        double stepT = 0.5 * stepX * stepX / 2;
         public Form1()
         {
             InitializeComponent();
@@ -29,50 +26,129 @@ namespace FDM
         private void button2_Click(object sender, EventArgs e)
         {
             timer.Stop();
-            textBox1.Text = ((Maxtemp - MinTemp) * 0.5).ToString();
+            TextBoxAmplitude.Text = ((Maxtemp - MinTemp) * 0.5).ToString();
+            TextBoxAmplitude1.Text = ((Maxtemp1 - MinTemp1) * 0.5).ToString();
         }
 
         private double Func(double t)
         {
             return 300 + 2 * Math.Sin(2 * Math.PI * 7 * t);
         }
-        private void button1_Click(object sender, EventArgs e)
-        {
-            ChartTX.ChartAreas[0].AxisY.Minimum = 297;
-            ChartTX.ChartAreas[0].AxisY.Maximum = 303;
-            ChartTt.ChartAreas[0].AxisY.Minimum = 299.95;
-            ChartTt.ChartAreas[0].AxisY.Maximum = 300.05;
-            for (int i = 0; i < numpoints; i++)
-            {
-                data[i] = T;
-                ChartTX.Series[0].Points.AddXY(i*stepX, data[i]);
-            }
-            timer.Interval = 1;
-            timer.Tick += (o, args) => timer_Tick(o,args);
-            timer.Start();
-        }
-        void timer_Tick(object sender, EventArgs e)
+        //Явный метод сеток
+        private void FiniteDifference()
         {
             time += stepT;
             previosTemp = data[0];
             data[0] = Func(time);
             ChartTX.Series[0].Points.Clear();
-            for(int i = 1; i < numpoints-1; i++)
+            for (int i = 1; i < numpoints - 1; i++)
             {
                 previosTemp1 = data[i];
-                data[i]=0.5*previosTemp + 0.5 * data[i + 1];
-                ChartTX.Series[0].Points.AddXY(i*stepX, data[i]);
-                previosTemp=previosTemp1;
+                data[i] = 0.5 * previosTemp + 0.5 * data[i + 1];
+                ChartTX.Series[0].Points.AddXY(i * stepX, data[i]);
+                previosTemp = previosTemp1;
             }
-            if (time > 8.5)
+            if (time > 6)
             {
-                ChartTt.Series[0].Points.AddXY(time, data[numpoints/5]);
-            
-                if (data[numpoints / 5] > Maxtemp)
+                ChartTt.Series[0].Points.AddXY(time, data[numpoints / 10]);
+
+                if (data[numpoints / 10] > Maxtemp)
                     Maxtemp = data[numpoints / 10];
-                if (data[numpoints / 5] < MinTemp)
+                if (data[numpoints / 10] < MinTemp)
                     MinTemp = data[numpoints / 10];
             }
         }
+        //Неявный метод сеток
+        private void ImplictFiniteDifference()
+        {
+            time1 += stepT1;
+            data1[0] = Func(time1);
+            a[1] = 1 / (2 + s);
+            b[1] = (data1[0] + s * data1[1]) / (2 + s);
+            for (int i = 2; i < numpoints - 1; i++)
+            {
+                a[i] = 1 / (2 + s - a[i - 1]);
+                b[i] = (b[i - 1] + s * data1[i]) / (2 + s - a[i - 1]);
+            }
+            for (int i = numpoints - 2; i >= 2; i--)
+            {
+                data1[i - 1] = a[i - 1] * data1[i] + b[i - 1];
+            }
+            ChartTX1.Series[0].Points.Clear();
+            for (int i = 0; i < numpoints; i++)
+                ChartTX1.Series[0].Points.AddXY(i * stepX, data1[i]);
+            if (time1 > 6)
+            {
+                ChartTt1.Series[0].Points.AddXY(time1, data1[numpoints / 10]);
+
+                if (data1[numpoints / 10] > Maxtemp1)
+                    Maxtemp1 = data1[numpoints / 10];
+                if (data1[numpoints / 10] < MinTemp1)
+                    MinTemp1 = data1[numpoints / 10];
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            time = 0;
+            time1 = 0;
+            
+
+            if (checkBoxPrint.Checked)
+            {
+                MinTemp = 350;
+                Maxtemp = 0;
+                ChartTX.Series[0].Points.Clear();
+                ChartTX.ChartAreas[0].AxisY.Minimum = 297;
+                ChartTX.ChartAreas[0].AxisY.Maximum = 303;
+                ChartTt.Series[0].Points.Clear();
+                ChartTt.ChartAreas[0].AxisY.Minimum = 299.9;
+                ChartTt.ChartAreas[0].AxisY.Maximum = 300.1;
+                for (int i = 0; i < numpoints; i++)
+                {
+                    data[i] = T;
+                    ChartTX.Series[0].Points.AddXY(i * stepX, data[i]);
+                }
+            }
+
+
+            if (checkBoxPrint1.Checked)
+            {
+                MinTemp1 = 350;
+                Maxtemp1 = 0;
+                ChartTX1.Series[0].Points.Clear();
+                ChartTX1.ChartAreas[0].AxisY.Minimum = 297;
+                ChartTX1.ChartAreas[0].AxisY.Maximum = 303;
+                ChartTt1.Series[0].Points.Clear();
+                ChartTt1.ChartAreas[0].AxisY.Minimum = 299.9;
+                ChartTt1.ChartAreas[0].AxisY.Maximum = 300.1;
+                if (textBoxT.Text != "")
+                    stepT1 = Convert.ToDouble(textBoxT.Text);
+                else
+                {
+                    stepT1 = stepT;
+                    textBoxT.Text = stepT1.ToString();
+                }
+
+                s = stepX * stepX / (stepT1 * D);
+                textBoxS.Text = s.ToString();
+                for (int i = 0; i < numpoints; i++)
+                {
+                    data1[i] = T;
+                    ChartTX1.Series[0].Points.AddXY(i * stepX, data[i]);
+                }
+            }
+
+            timer.Interval = 1;
+            timer.Tick += (o, args) => timer_Tick(o, args);
+            timer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (checkBoxPrint.Checked)
+                FiniteDifference();
+            if (checkBoxPrint1.Checked)
+                ImplictFiniteDifference();
+
         }
     }
+}
